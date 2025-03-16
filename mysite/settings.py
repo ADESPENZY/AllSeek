bf0fc0282bf206a2c299f361dae0f50bcdeaf810
Joshua Atoyebi <atoyebijoshua095@gmail.com>
2025-03-16 12:17:37 -0500

    Add whitenoise

--- mysite/settings.py
+++ mysite/settings.py
@@ -59,6 +59,7 @@ NPM_BIN_PATH = "C:/Program Files/nodejs/npm.cmd"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -140,6 +141,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",  # Adjust path according to your structure
 ]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
